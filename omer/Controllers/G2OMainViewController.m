f7eb54fcf60a0264b5d9030dfde3b79369583bc8
yaakov <me@yaakov.io>
2015-04-17 14:12:42 +0300

    adds omer today extension

--- omer/Controllers/G2OMainViewController.m
+++ omer/Controllers/G2OMainViewController.m
@@ -9,7 +9,7 @@
 #import "G2OMainViewController.h"
 #import "Calendar.h"
 #import "KCSefiratHaomerCalculator.h"
-
+#import <AVFoundation/AVFoundation.h>
 
 @interface G2OMainViewController ()
 
@@ -17,6 +17,7 @@
 @property (strong, nonatomic) IBOutlet UILabel *counterLabel;
 @property (strong, nonatomic) IBOutlet UILabel *dayWeekCountLabel;
 @property (nonatomic,assign) NSInteger currentCount;
+@property (nonatomic, strong) AVPlayerLayer *playerLayer;
 
 @end
 
@@ -30,16 +31,61 @@
     
     [self setupDayWeekLabel];
     
+    // find movie file
+//    NSString *moviePath = [[NSBundle mainBundle] pathForResource:@"corn" ofType:@"mov"];
+//    NSURL *movieURL = [NSURL fileURLWithPath:moviePath];
+//    self.playerLayer = [AVPlayerLayer playerLayerWithPlayer:[[AVPlayer alloc]initWithURL:movieURL]];
+////    self.playerLayer.frame = CGRectMake(0,0,self.view.bounds.size.width, self.view.bounds.size.height);
+//    self.playerLayer.frame = CGRectMake(0,0,400, 1024);
+//    self.playerLayer.backgroundColor = (__bridge CGColorRef)([UIColor redColor]);
+    
+    
+
+    
 //    [[NSTimer scheduledTimerWithTimeInterval:0.5
 //                                     target:self
 //                                   selector:@selector(updateLabels)
 //                                   userInfo:nil
 //                                    repeats:YES] fire];
+//    [self.view.layer addSublayer:self.playerLayer];
+//    
+//    // loop movie
+//    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(replayMovie:) name:AVPlayerItemDidPlayToEndTimeNotification object:nil];
+    
+    
 }
 
-- (void)viewWillDisappear:(BOOL)animated {
-    [super viewWillDisappear:animated];
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    if (self.playerLayer) {
+        [self.playerLayer.player play];
+    }
 }
+//-(AVPlayerLayer*)playerLayer{
+//    if(!_playerLayer){
+//        
+//        // find movie file
+//        NSString *moviePath = [[NSBundle mainBundle] pathForResource:@"corn" ofType:@"mov"];
+//        NSURL *movieURL = [NSURL fileURLWithPath:moviePath];
+//        _playerLayer = [AVPlayerLayer playerLayerWithPlayer:[[AVPlayer alloc]initWithURL:movieURL]];
+//        _playerLayer.frame = CGRectMake(0,0,self.view.frame.size.width, self.view.frame.size.height);
+//        [_playerLayer.player play];
+//        
+//        return _playerLayer;
+//    }
+//    
+//    return _playerLayer;
+//}
+
+//-(void)replayMovie:(NSNotification *)notification
+//{
+//    [self.playerLayer.player play];
+//}
+//
+//- (void)viewWillDisappear:(BOOL)animated {
+//    [super viewWillDisappear:animated];
+//}
 
 #pragma mark - Helper methods
 
