419cf1bc6c71418252b74a7ca262b246b7eb78de
yaakov <me@yaakov.io>
2015-04-23 16:20:19 +0300

    watch template code

---
+++ omer WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  omer WatchKit Extension
+//
+//  Created by Yaakov Gamliel on 4/23/15.
+//  Copyright (c) 2015 Yaakov Gamliel. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
